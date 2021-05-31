d23ab15be2f296048854ca2381ec92aeb1e8f6b1
cxan96 <c.p.xanten@gmail.com>
2021-05-31 14:54:13 +0200

    added scikit-learn dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="oblivious-sketching-logreg",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["numba", "scipy"],
+    install_requires=["numba", "scikit-learn", "scipy"],
 )
