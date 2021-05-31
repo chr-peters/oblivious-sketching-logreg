9791964a6131e66047cf9cee3b81567b23f3118e
cxan96 <c.p.xanten@gmail.com>
2021-05-31 14:24:10 +0200

    added empty package + configured tooling

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="oblivious-sketching-logreg",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["numba", "scipy"],
+)
