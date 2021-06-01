98c217b92f43a9151098dd35f3890674bb4b04a2
cxan96 <c.p.xanten@gmail.com>
2021-06-01 13:58:56 +0200

    don't create directories on import time

--- sketching/settings.py
+++ sketching/settings.py
@@ -4,11 +4,7 @@ BASE_DIR = Path(__file__).parent.parent
 
 # the downloaded datasets will go here
 DATA_DIR = BASE_DIR / ".data-cache"
-if not DATA_DIR.exists():
-    DATA_DIR.mkdir()
 
 RESULTS_DIR = BASE_DIR / "experimental-results"
-if not RESULTS_DIR.exists():
-    RESULTS_DIR.mkdir()
 
 LOGGER_NAME = "sketching"
