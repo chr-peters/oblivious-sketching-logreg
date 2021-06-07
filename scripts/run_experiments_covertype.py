fddfa1a107cd7e21543e8cbb999996b4c2e505dc
cxan96 <c.p.xanten@gmail.com>
2021-06-07 13:52:00 +0200

    added script to run covertype experiments

---
+++ scripts/run_experiments_covertype.py
@@ -0,0 +1,23 @@
+import logging
+
+from sketching import settings
+from sketching.datasets import Covertype_Sklearn
+from sketching.utils import run_experiments
+
+logger = logging.getLogger(settings.LOGGER_NAME)
+
+
+MIN_SIZE = 500
+MAX_SIZE = 15000
+STEP_SIZE = 500
+NUM_RUNS = 2
+
+dataset = Covertype_Sklearn()
+
+run_experiments(
+    dataset=dataset,
+    min_size=MIN_SIZE,
+    max_size=MAX_SIZE,
+    step_size=STEP_SIZE,
+    num_runs=NUM_RUNS,
+)
